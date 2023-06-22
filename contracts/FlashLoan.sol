43883ec6c67da4d6a768319453bf73bca6e2b953
PavanTamrakar <thepavantamrakar@gmail.com>
2023-06-22 17:09:00 +0530

    code

---
+++ contracts/FlashLoan.sol
@@ -0,0 +1,10 @@
+//SPDX-Licence-Idntifier: MIT
+pragma solidity ^0.8.10;
+
+import {FlashLoanSimpleReceiverBase} from "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract FlashLoan is FlashLoanSimpleReceiverBase {
+    
+}
