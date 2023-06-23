b90cd4ce76a57c9d99a894250277d61dee95575e
PavanTamrakar <thepavantamrakar@gmail.com>
2023-06-23 16:57:49 +0530

    createFlashLoan

--- contracts/FlashLoan.sol
+++ contracts/FlashLoan.sol
@@ -6,5 +6,18 @@ import {FlashLoanSimpleReceiverBase} from "@aave/core-v3/contracts/flashloan/bas
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract FlashLoan is FlashLoanSimpleReceiverBase {
-    
+
+    event log(address asset, uint265 val);
+
+    constructor(IPoolAddressProvider provider)
+        FlashLoanSimpleReceiverBase(provider)
+    {}
+
+    function createFlashLoan(address asset, uint256 amount) external {
+        address receiver = address(this);
+        bytes memory params = "";
+        uint256 referralCode = 0;
+
+        POOL.flashLoanSimple(receiver, asset, amount, params, referralCode);
+    }
 }
